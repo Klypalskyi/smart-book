9c495f30a94ccb7af2838f21d3ecb5c2c244cddc
Klypalskyi <klypalskyi@gmail.com>
2019-11-25 21:44:07 +0200

    initial repo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Containers/App/App';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import store from './Redux/store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
