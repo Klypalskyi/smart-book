85150db2ac3e363e8ddaea5016fd2653ca304a13
Viktor Bulvarenko <3513401+vbguard@users.noreply.github.com>
2019-11-27 23:26:43 +0200

    Update App.jsx

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import FormOfRegistration from '../../components/FormOfRegistration/FormOfRegistration';
-
 // import PropTypes from 'prop-types'
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <FormOfRegistration />
       {/* ... bottom place your components */}
     </>
   );
