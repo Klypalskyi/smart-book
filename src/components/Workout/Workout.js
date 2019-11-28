d7c9a32efafe1526f8515ed12d758920d3a6320a
Anna Kholod <uashop89@gmail.com>
2019-11-28 22:23:29 +0200

    added workout

---
+++ src/components/Workout/Workout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Workout.module.css';
+
+const Workout = () => {
+  return <div className={style.container}>content</div>;
+};
+
+export default Workout;
