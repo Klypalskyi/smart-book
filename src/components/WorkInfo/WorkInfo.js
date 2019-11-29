34368b444fbf562fd151b5f30b85a82ab6634644
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-30 00:22:37 +0200

    page-work-mobile_style, mobyle/tablet/desktop done

--- src/components/WorkInfo/WorkInfo.js
+++ src/components/WorkInfo/WorkInfo.js
@@ -3,71 +3,9 @@ import css from './WorkInfo.module.css';
 
 const WorkInfo = () => {
   return (
-    <>
-      <header className={css.header}>
-        <h1>WorkInfo Page</h1>
-      </header>
-      <div className={css.container}>
-        <div className={css.inside}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-          distinctio sint cumque id animi optio? Corrupti nam mollitia ipsum
-          nemo officiis vero est id commodi at dolore, dolores sequi, iusto
-          facilis aspernatur tempore eum velit ipsa voluptatem temporibus minus
-          totam. Laudantium nam, aperiam quasi odit similique eligendi optio
-          sapiente quae sit ipsa error voluptatem fugit modi minus, molestiae
-          placeat eos, sunt officia pariatur velit. Corrupti ullam explicabo
-          alias vel ea! Quae minima laudantium qui fuga nobis fugiat. Asperiores
-          harum nemo pariatur ullam quod optio repellendus beatae. Quam,
-          exercitationem voluptates temporibus tempora nihil, laborum expedita
-          nesciunt mollitia libero voluptate autem. Excepturi consectetur
-          accusantium, sit, animi quibusdam accusamus unde laboriosam sunt
-          deserunt at fugiat odio praesentium quis libero consequuntur commodi.
-          Vero numquam, recusandae sunt nobis esse incidunt nemo laborum
-          suscipit ab odit soluta sed quam asperiores porro fugit expedita?
-          Perspiciatis blanditiis harum incidunt magni esse, aut laborum
-          consequatur velit mollitia facere distinctio, sed eaque earum
-          consequuntur nulla accusantium, illo voluptas sint iste recusandae
-          delectus illum eveniet? Molestiae aspernatur quo blanditiis maiores
-          incidunt, aperiam exercitationem odit aliquam, quisquam, distinctio
-          nulla voluptatem deserunt ut consequatur beatae deleniti pariatur
-          ipsum praesentium atque? Veniam itaque recusandae nostrum voluptate,
-          voluptas voluptatem? Aut harum quis vero nesciunt? Numquam,
-          cupiditate? Nemo quo deleniti modi minima laborum veritatis nesciunt
-          reiciendis in dolore. Aut voluptas consectetur sint. Rem maxime fuga
-          eum nemo repudiandae vero consequatur sit. Consequatur fugiat
-          voluptas, delectus accusantium earum autem non adipisci quibusdam,
-          quidem quis possimus! Ipsum, nobis neque amet unde error nulla sunt
-          molestias voluptate quisquam consequatur nihil consectetur voluptatum
-          quae minus aliquid at minima asperiores officia ad. Nisi reprehenderit
-          fugit ducimus expedita commodi consequuntur voluptas saepe officia.
-          Veritatis vel quos ad doloremque velit perferendis magnam ex ratione,
-          cupiditate nihil exercitationem voluptatem tempora esse sit
-          dignissimos dolorem saepe magni suscipit maxime blanditiis. Deleniti
-          temporibus neque delectus ut voluptate accusamus, quod molestias
-          nesciunt assumenda eveniet provident odio commodi dicta laboriosam
-          impedit numquam necessitatibus, ab reprehenderit ea! Ratione assumenda
-          dolore ducimus itaque distinctio placeat voluptatum beatae delectus,
-          eos ullam accusantium maxime eum labore vero dolor nulla omnis sunt
-          quam, porro quae ea ipsa soluta rerum amet. Voluptate earum nihil
-          reiciendis, autem dolores reprehenderit est et, distinctio adipisci
-          mollitia voluptas quae repudiandae non ipsa, aspernatur nesciunt.
-          Ipsum cum nesciunt sed repudiandae adipisci commodi fugiat placeat
-          similique ducimus, iusto ratione beatae tenetur laudantium facere
-          quisquam sunt reprehenderit nulla distinctio assumenda consectetur rem
-          praesentium? Ducimus, quis possimus harum aperiam excepturi tempore
-          quam voluptatem ipsam, recusandae quos doloremque, ad accusantium ut
-          eligendi. Adipisci labore, eos, sunt harum ratione fugiat suscipit
-          perferendis praesentium, sequi est asperiores dignissimos odit iusto
-          tempora unde aspernatur vitae sed recusandae? Culpa, id nihil
-          voluptate dolor officiis assumenda sed nulla unde distinctio at ipsa!
-          Doloremque accusamus illo laudantium et id beatae molestiae dolor.
-          Animi aliquam, voluptatibus ratione tempora illo dignissimos molestias
-          voluptate dolore tempore alias, iusto odio. Officia cumque in totam
-          laborum placeat. Explicabo impedit vel aliquid quia in rem incidunt
-          sunt quasi fugit minus nobis cupiditate veritatis laborum.
-        </div>
-      </div>
-    </>
+    <div className={css.page}>
+      <div className={css.insideContainer} />
+    </div>
   );
 };
 
