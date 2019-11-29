f99c81284f2ecd9e919df7ffd1b50a205372cb95
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-30 00:31:38 +0200

    page-work-mobile_style, mobyle/tablet/desktop done

--- src/сontainers/App/App.jsx
+++ src/сontainers/App/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // import PropTypes from 'prop-types'
 
-import WorkInfo from '../../components/WorkInfo/WorkInfo';
-
 function App() {
   return (
     <>
