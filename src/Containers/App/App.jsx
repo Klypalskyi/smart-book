71708c7ffba354829cdbfeaec2f5d908fc4a6fb4
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-29 23:18:40 +0200

    page-work-mobile_style, not finished

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // import PropTypes from 'prop-types'
 
+import WorkInfo from '../../components/WorkInfo/WorkInfo';
+
 function App() {
   return (
     <>
       <CssBaseline />
       {/* ... bottom place your components */}
+
+      <WorkInfo />
     </>
   );
 }
