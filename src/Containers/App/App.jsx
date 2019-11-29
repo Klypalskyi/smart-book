99d4471637aac27147fb265154a5ebc8b00578d5
Andriy Kyrylenko <z9877969@ukr.net>
2019-11-29 23:25:54 +0200

    deleted import form-of-ragistration in app-component

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import FormOfRegistration from '../../components/FormOfRegistration/FormOfRegistration';
 // import PropTypes from 'prop-types'
 
 function App() {
