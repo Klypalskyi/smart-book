b4e09502b859f39469161b7e184da1731001a9ad
Lena <2863040@ukr.net>
2019-11-29 17:32:39 +0200

    added formik and validation

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import LoginPage from '../../components/LoginPage/LoginPage';
 
 // import PropTypes from 'prop-types'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <CssBaseline />
       {/* ... bottom place your components */}
+      <LoginPage />
     </>
   );
 }
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,32 +1,66 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import styles from './LoginPage.module.css';
 
-const LoginPage = () => (
-  <form className={styles.form}>
-    <label className={styles.label} htmlFor="email">
-      <p className={styles.emailText}>Електронна адреса</p>
-      <input
-        type="email"
-        placeholder="your@email.com"
-        className={styles.emailInput}
-      />
-    </label>
-    <label className={styles.label} htmlFor="password">
-      <p className={styles.passwordText}>Пароль</p>
-      <input
-        type="password"
-        placeholder="Пароль"
-        className={styles.passwordInput}
-      />
-    </label>
-    <button type="submit" className={styles.logInButton}>
-      Войти
-    </button>
-    <Link to="/registration" className={styles.singInLink}>
-      Реєстрація
-    </Link>
-  </form>
-);
+const LoginPage = ({ onLogIn }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Email обовязкове поле'),
+      password: Yup.string()
+        .min(6, 'Пароль має бути не менше 6 символів')
+        .required('Password обовязкове поле'),
+    }),
+    onSubmit: values => {
+      JSON.stringify(values, null, 2);
+      const credential = {
+        email: values.email,
+        password: values.password,
+      };
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit} className={styles.form}>
+      <label className={styles.label} htmlFor="email">
+        <p className={styles.emailText}>Електронна адреса</p>
+        <input
+          placeholder="your@email.com"
+          type="email"
+          {...formik.getFieldProps('email')}
+          className={styles.emailInput}
+        />
+      </label>
+      {formik.touched.email && formik.errors.email ? (
+        <span className={styles.emailError}>{formik.errors.email}</span>
+      ) : null}
+      <label className={styles.label} htmlFor="password">
+        <p className={styles.passwordText}>Пароль</p>
+        <input
+          placeholder="Пароль"
+          type="password"
+          {...formik.getFieldProps('password')}
+          className={styles.passwordInput}
+        />
+      </label>
+      {formik.touched.password && formik.errors.password ? (
+        <span className={styles.passwordError}>{formik.errors.password}</span>
+      ) : null}
+      <button type="submit" className={styles.logInButton}>
+        Увійти
+      </button>
+      <Link to="/registration" className={styles.singInLink}>
+        Реєстрація
+      </Link>
+    </form>
+  );
+};
 
-export default LoginPage;
+const mapDispatchToProps = {};
+
+export default connect(null, mapDispatchToProps)(LoginPage);
