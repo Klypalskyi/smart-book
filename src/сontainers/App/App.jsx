ed4cab2af433bc9b6b07c8a19f3294f7014e0f03
Vlad Shum <shumanskyivlad@gmail.com>
2019-11-30 01:28:42 +0200

    BooksList_mobile_stile

--- src/сontainers/App/App.jsx
+++ src/сontainers/App/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import BooksList from '../../components/BooksList/BooksList';
 // import PropTypes from 'prop-types'
 
 function App() {
   return (
     <>
       <CssBaseline />
-
+      <BooksList />
       {/* ... bottom place your components */}
     </>
   );
