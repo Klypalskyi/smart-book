3489b621864e48722385d3467d89e0430ed0fc04
Klypalskyi <klypalskyi@gmail.com>
2019-11-30 17:58:48 +0200

    backdrop with redux actions

--- src/сontainers/App/App.jsx
+++ src/сontainers/App/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Backdrop from '../../components/Backdrop'
+import FormOfRegistration from '../../components/FormOfRegistration/FormOfRegistration'
 // import PropTypes from 'prop-types'
 
-function App() {
+function App () {
   return (
     <>
       <CssBaseline />
-
+      <Backdrop component={FormOfRegistration} />
       {/* ... bottom place your components */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
