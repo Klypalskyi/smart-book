98640534da99de5ee3d15b4ed2780e598f5c5009
Viktor Bulvarenko <silken.ua@gmail.com>
2019-12-01 21:19:25 +0200

    add training page with timer and results components

--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import style from './TrainingPage.module.css';
+import Timer from '../../components/Timer/Timer';
+import Results from '../../components/Results/Results';
 
 const TrainingPage = () => {
-  return <div className={style.container} />;
+  return (
+    <div className={style.container}>
+      <Timer />
+      <Results />
+    </div>
+  );
 };
 
 export default TrainingPage;
