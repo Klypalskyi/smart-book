ac7eccfb5e8f6e2a80991b96435d49454bcfdf0e
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-12-03 22:00:40 +0200

    Goal component fixed, Add Goal component to Training Page

--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import style from './TrainingPage.module.css';
 import PanelOfTimers from '../../components/Timer/PanelOfTimers';
 import Results from '../../components/Results/Results';
+import Goal from '../../components/Goal/Goal';
 
 const TrainingPage = () => {
   return (
     <div className={style.container}>
+      <Goal />
       <PanelOfTimers />
       <Results />
     </div>
