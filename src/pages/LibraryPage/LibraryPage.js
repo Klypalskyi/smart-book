f21fb65a22a9bdb04cb7ef0dbd55c830016b033a
Viktor2350 <ryjuk39@gmail.comm>
2019-12-05 00:29:23 +0200

    LibraryPage created

--- src/pages/LibraryPage/LibraryPage.js
+++ src/pages/LibraryPage/LibraryPage.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+// import { Redirect } from 'react-router-dom';
 import styles from './LibraryPage.module.css';
 import AddBook from '../../components/AddBook/AddBook';
 import PlanReadBooks from '../../components/PlanReadBooks/PlanReadBooks';
@@ -7,8 +10,29 @@ import ReadBooks from '../../components/ReadBooks/ReadBooks';
 import NextStepButton from '../../components/NextStepButton/NextStepButton';
 
 const LibraryPage = () => {
+  const [books, setBooks] = useState(null);
+  const token = useSelector(state => state.session.token);
+  useEffect(() => {
+    const BaseUrl = 'https://smart-book.goit.co.ua/api/v1/books';
+    axios
+      .get(BaseUrl, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then(res => {
+        // setBooks(...res.books);
+        setBooks(res.books);
+        console.log(res);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  console.log('books:', books);
+
   return (
     <div className={styles.libraryPage__wrapper}>
+      {/* {books.length !== '0' && <Redirect to="/training" />} */}
       <AddBook />
       <NextStepButton />
       <ReadBooks />
