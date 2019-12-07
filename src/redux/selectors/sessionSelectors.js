e9e62a63b2ca48a4e23c6776d0f6cc48b91245e6
VladDlogush <vlad.dlogush21@gmail.com>
2019-12-07 12:50:02 +0200

    fix components

--- src/redux/selectors/sessionSelectors.js
+++ src/redux/selectors/sessionSelectors.js
@@ -2,4 +2,3 @@ export const getUser = state => state.user;
 export const getUserToken = state => state.session.token;
 export const getIsModalOpen = state => state.isModalOpen;
 export const getAuthenticated = state => state.session.authenticated;
-export const getFullName = state => state.user.name.fullName;
