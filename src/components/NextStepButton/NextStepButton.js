25a4ca7e6b3f798dfb427a5a3402b766f5b713c7
Viktor2350 <ryjuk39@gmail.comm>
2019-12-07 17:51:13 +0200

    fixed NextStepBtn

--- src/components/NextStepButton/NextStepButton.js
+++ src/components/NextStepButton/NextStepButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './NextStepButton.module.css';
 
 const NextStepButton = () => {
   return (
     <div className={styles.nextStepButton__wrapper}>
-      <button type="button" className={styles.nextStepButton__button}>
-        <NavLink to="/training">Сформувати тренування</NavLink>
-      </button>
+      <Link to="/training" className={styles.nextStepButton__button}>
+        Сформувати тренування
+      </Link>
     </div>
   );
 };
