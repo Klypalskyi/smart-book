8037dc0987f1f3b491f373bb5bf3449900eac8d1
VladDlogush <vlad.dlogush21@gmail.com>
2019-12-07 10:21:36 +0200

    delete headerContainer

--- src/components/Header/HeaderContainer.jsx
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { getUser } from '../../redux/selectors/sessionSelectors';
-import Header from './Header';
-
-const mapStateToProps = state => ({
-  userName: getUser(state),
-});
-
-export default connect(mapStateToProps, null)(Header);
