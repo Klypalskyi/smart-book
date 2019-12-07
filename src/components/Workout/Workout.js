68a53a06ad23367be9da635ee22df8ab7951306d
Anna Kholod <uashop89@gmail.com>
2019-12-07 10:13:16 +0200

    paste root reducer

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import style from './Workout.module.css';
+import TrainingBookTable from '../TrainingBooksTable/TrainingBooksTable';
 
 const Workout = () => {
-  return <div className={style.container}>content</div>;
+  // const [books, setBooks] = useState({});
+  // const [timeStart, setTimeStart] = useState();
+  // const [timeEnd, setTimeEnd] = useState();
+  // const [avgReadPages, setAvgReadPages] = useState(0);
+
+  return (
+    <div className={style.container}>
+      <TrainingBookTable />
+    </div>
+  );
 };
 
 export default Workout;
--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import style from './TrainingPage.module.css';
 import PanelOfTimers from '../../components/Timer/PanelOfTimers';
 import Results from '../../components/Results/Results';
-import Goal from '../../components/Goal/Goal';
+import ModalCongrats from '../../components/ModalCongrats/ModalCongrats';
+// import { addGoal } from '../../redux/goal/goalActions';
+import Workout from '../../components/Workout/Workout';
 
-const TrainingPage = () => {
+// const URL = 'https://book-read.goit.co.ua/api/v1';
+
+const TrainingPage = ({ modalCongratsOpen }) => {
+  useEffect(() => {
+    // fetch(URL)
+    //   .then(res => res.json())
+    //   .then(data => addGoal(data.hits));
+  }, []);
+
   return (
     <div className={style.container}>
-      <Goal />
+      {/* <Goal /> */}
       <PanelOfTimers />
       <Results />
+      {modalCongratsOpen && <ModalCongrats />}
+      <Workout />
     </div>
   );
 };
 
-export default TrainingPage;
+const mapStateToProps = state => ({
+  modalCongratsOpen: state.componentController.modalCongratsOpen,
+});
+
+TrainingPage.propTypes = {
+  modalCongratsOpen: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps, null)(TrainingPage);
